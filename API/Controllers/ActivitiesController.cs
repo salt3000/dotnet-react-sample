b1a13e75d13fd793b71f27c8f4dca7465c23e484
a.e <dv123lp@gmail.com>
2022-10-25 00:58:22 +0900

    add edit handler

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -11,24 +11,31 @@ using Persistence;
 
 namespace API.Controllers
 {
-    public class ActivitiesController : BaseApiController
+  public class ActivitiesController : BaseApiController
+  {
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetActivities()
-        {
-            return await Mediator.Send(new List.Query());
-        }
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Activity>> GetActivity(Guid id)
-        {
-            return await Mediator.Send(new Details.Query() { Id = id });
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> CreateActivity([FromBody]Activity activity)
-        {
-            return Ok(await Mediator.Send(new Create.Command() { Activity = activity }));
-        }
+      return await Mediator.Send(new List.Query());
     }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivity(Guid id)
+    {
+      return await Mediator.Send(new Details.Query() { Id = id });
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateActivity([FromBody] Activity activity)
+    {
+      return Ok(await Mediator.Send(new Create.Command() { Activity = activity }));
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+    {
+      activity.Id = id;
+      return Ok(await Mediator.Send(new Edit.Command() { Activity = activity }));
+    }
+  }
 }
