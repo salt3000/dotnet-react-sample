f236e61febcdf9b45f1cc03299ea19236eb0f7aa
a.e <dv123lp@gmail.com>
2022-10-25 00:41:01 +0900

    add create handler

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -11,20 +11,24 @@ using Persistence;
 
 namespace API.Controllers
 {
-  public class ActivitiesController : BaseApiController
-  {
-
-
-    [HttpGet]
-    public async Task<ActionResult<List<Activity>>> GetActivities()
+    public class ActivitiesController : BaseApiController
     {
-        return await Mediator.Send(new List.Query());
-    }
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await Mediator.Send(new List.Query());
+        }
 
-    [HttpGet("{id}")]
-    public async Task<ActionResult<Activity>> GetActivity(Guid id)
-    {
-        return await Mediator.Send(new Details.Query(){ Id = id });
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await Mediator.Send(new Details.Query() { Id = id });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity([FromBody]Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command() { Activity = activity }));
+        }
     }
-  }
 }
